167c68c0793e28c10cf00373852ed404c399163a
root <root@LAPTOP-9EALPA8K>
2023-05-31 21:40:42 +0700

    new

---
+++ pert4/no4.cc
@@ -0,0 +1,16 @@
+#include <iostream>
+
+using namespace std;
+
+int main()
+    int n, _, i, _;
+    cout <<"masukkan jumlah baris : ";
+    cin >> n;
+        for (i = 1; i<= _; i++)
+        {
+            for (s = i; _ <n; _++)
+            cout << "";
+            for (j = 1; _<= i; _++)
+            cout << "*";
+            cout << "\n"
+        }
---
+++ strukturdata/struct3.cc
@@ -0,0 +1,52 @@
+#include <iostream>
+#include <string>
+using namespace std;
+ 
+struct mahasiswa{
+    string nama;
+    string nim;
+    string fakulta;
+    string jurusan;
+    string kota_asal;
+    string alamat;
+};
+
+int main()
+{
+  cout << "##  Program C++ Biodata Mahasiswa  ##" << endl;
+  cout << "====================================" << endl;
+  cout << endl;
+ 
+  string nama, nim, fakultas, jurusan, kota_asal, alamat;
+ 
+  cout << "Nama mahasiswa: ";
+  getline(cin,nama);
+ 
+  cout << "NIM: ";
+  getline(cin,nim);
+ 
+  cout << "Fakultas: ";
+  getline(cin,fakultas);
+ 
+  cout << "Jurusan: ";
+  getline(cin,jurusan);
+ 
+  cout << "Kota Asal: ";
+  getline(cin,kota_asal);
+ 
+  cout << "Alamat: ";
+  getline(cin,alamat);
+ 
+  cout << endl;
+ 
+  cout << "# Data Mahasiswa #"       << endl;
+  cout << "=================="       << endl;
+  cout << "Nama: "      << nama      << endl;
+  cout << "NIM: "       << nim       << endl;
+  cout << "Fakultas: "  << fakultas  << endl;
+  cout << "Jurusan: "   << jurusan   << endl;
+  cout << "Kota Asal: " << kota_asal << endl;
+  cout << "Alamat: "    << alamat    << endl;
+ 
+  return 0;
+}
